d4611bc2122cd107ed1ba43f39c5558af427eeed
flavorzhu <haker-haker@163.com>
2012-08-23 22:05:57 +0800

    init project

---
+++ src/main/java/com/zhuyanbin/excel2xml/App.java
@@ -0,0 +1,13 @@
+package com.zhuyanbin.excel2xml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
